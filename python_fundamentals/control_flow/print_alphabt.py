c07a03dd2c750228c7c9afa0b44b3dd9c9c33c86
Bandar1414 <bandor141414@gmail.com>
2026-09-19 21:52:25 +0300

    Fix print_alphabt exact output

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""Print the lowercase alphabet except q and e."""
-
-for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):
-        print("{}".format(chr(letter)),
-              end="\n" if letter == ord('z') else "")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
